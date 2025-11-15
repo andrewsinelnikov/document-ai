552810d54beb457fe2239900e89fa2866731d9ae
Andrew Sinelnikov <ansinelnikov@gmail.com>
2025-11-15 21:57:18 +0200

    add App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+// import Footer from './components/Footer';
+import Home from './pages/Home';
+// import ContractFlow from './pages/ContractFlow';
 
+export default function App() {
   return (
-    <>
-      <>App component</>
-    </>
-  )
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/contract" element={<ContractFlow />} /> */}
+          </Routes>
+        </main>
+        {/* <Footer /> */}
+      </div>
+    </BrowserRouter>
+  );
 }
-
-export default App
