82c39b1242be753d9c17916f3644d4fb431fb3b7
Andrew Sinelnikov <ansinelnikov@gmail.com>
2025-11-18 22:51:08 +0200

    added  ContractChatBot in suflowe

---
+++ frontend/src/pages/ContractChatBot.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContractChatBot = () => {
+  return (
+    <div>
+        <h1>hello from ContractChatBot</h1>
+    </div>
+  )
+}
+
+export default ContractChatBot
