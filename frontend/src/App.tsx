e31224c83479e5b0629ac20eb85511b774b8da3a
Andrew Sinelnikov <ansinelnikov@gmail.com>
2025-11-19 23:27:17 +0200

    add document

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import ContractFlow from './pages/ContractFlow';
+import ContractChatBot from './pages/ContractChatBot';
 import styles from './App.module.css';
 
 export default function App() {
@@ -13,7 +14,8 @@ export default function App() {
         <main className={styles.main}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/contract" element={<ContractFlow />} />
+            {/* <Route path="/contract" element={<ContractFlow />} /> */}
+            <Route path="/contract" element={<ContractChatBot />} />
           </Routes>
         </main>
         <Footer />
